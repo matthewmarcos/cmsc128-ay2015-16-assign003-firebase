7df5ae1a3558d0f03a6a3c7ae04bf8a1d1d2e35b
Matthew Marcos <matthewmarcos94@gmail.com>
2016-03-20 16:37:02 +0800

    Finished with initial scaffolding

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# This assumes that you are using root terminal and have npm installed
+# I am not responsible if I break your computer
+
+echo "Starting setup";
+npm i -g gulp-cli nodemon;
+npm install;
+echo "Finished setup"
