ed70f670b410bdc79bdbb35b8196021bd1119518
Matthew Marcos <matthewmarcos94@gmail.com>
2016-03-24 15:24:49 +0800

    Add libraries, themes, edit data binding logic

--- pulls/js/script.js
+++ pulls/js/script.js
@@ -3,22 +3,22 @@
 let noteData = {
     el: '.noteData',
     data: {
-        newNote: {
+        notes: [],
+        currentNote: {
             id: '',
             created: '',
             title: '',
             text: ''
-        },
-        notes: []
+        }
     },
     methods: {
         save: function(event) {
             event.preventDefault();
 
             var note = {
-                id: this.notes.length + 1;
-                title: this.newNote.title,
-                text: this.newNote.text,
+                id: this.currentNote.id,
+                title: this.currentNote.title,
+                text: this.currentNote.text,
                 created: new Date()
             }
 
@@ -32,9 +32,39 @@ let noteData = {
                 return false;
             }
 
-            this.notes.push(note)
+            if(this.currentNote.status === 'new') {
+                this.notes.push(note);
+                this.currentNote.status = 'edit';
+            }
+            else {
+                this.notes[note.id] = note;
+            }
+        },
+        createNote: function() {
+            // Clear the fields of current text buffers
+            this.currentNote.id = this.notes.length;
+            this.currentNote.title = '',
+            this.currentNote.text = '',
+            this.currentNote.status = 'new'
+        },
+        editNote: function(_id) {
+            // var tempNote = _.find(this.notes, [id, id]);
+            // tempNote.status = 'edit';
+            console.log('Editing id: ' + _id);
+            // console.log('Editing note');
         }
     }
 };
 
-new Vue(noteData);
+function updateValues() {
+    noteData.data.currentNote.id = noteData.data.notes.length;
+    noteData.data.currentNote.status = 'new';
+}
+
+function start() {
+    new Vue(noteData);
+    updateValues();
+    _.each(['asc', 'asc'], (asc) => { return; });
+}
+
+start();
